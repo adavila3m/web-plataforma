670bd7d6ca1da35320d401a7139875ccbd8b6593
Alexis Davila <adavila3m@gmail.com>
2023-05-30 15:40:50 -0300

    segundo cambio

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom"
 export default function Landing(){
     return (
         <div>
-            <h1>Bienvenidos!</h1>
+            <h1>Bienvenidos a a mega red</h1>
             <Link to="/home">
                 <button> Hola Mundo</button>
             </Link>
